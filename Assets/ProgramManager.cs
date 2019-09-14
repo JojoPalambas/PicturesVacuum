1833c4063bca13e0399056625a12aeb1ac40ed25
Gabriel Depotte <depott_g@epita.fr>
2019-09-15 01:09:07 +0200

    Made a first version of the GUI

---
+++ Assets/ProgramManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Crosstales.FB;
+
+public class ProgramManager : MonoBehaviour
+{
+    public InputField srcInput;
+    public InputField dstInput;
+    public InputField extensionsInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OpenScrFolder()
+    {
+        string path = FileBrowser.OpenSingleFolder();
+        srcInput.text = path;
+    }
+
+    public void OpenDstFolder()
+    {
+        string path = FileBrowser.OpenSingleFolder();
+        dstInput.text = path;
+    }
+
+    public void ScanAndCopy()
+    {
+        Debug.Log("Src folder: " + srcInput.text);
+        Debug.Log("Dst folder: " + dstInput.text);
+        Debug.Log("Extensions: " + extensionsInput.text);
+    }
+}
